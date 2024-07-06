d52e477316a080725249bb8403000c2a6bf5bb4e
Sonu7042 <sonusinghmourya100@gmail.com>
2024-07-06 17:30:43 +0530

    updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,27 +1,48 @@
 import React, { useState } from 'react'
 import Logo from '../assets/Pragati logo.png'
-import { FaBars } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
 
 const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
 
 
 
   return (
 
     <header className='bg-white h-30 shadow-sm  w-full z-50  fixed top-0 left-0 '  >
+
+
       <div className=' flex  w-full h-full items-center  mx-auto justify-between lg:flex-row flex-col mb-4'>
 
         {/* logo */}
         <Link to={'/'} className='flex items-center justify-center px-5   '>
-          <img  src={Logo} alt="" width={200} />
+          <img src={Logo} alt="" width={200} />
         </Link>
 
 
+        {/* Menu Icons for Mobile */}
+        <div className='absolute top-4 right-4 flex lg:hidden'>
+          <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
+            {
+              menuOpen ? <IoClose /> : <FaBars />
+
+            }
+          </button>
+
+        </div>
 
 
-
-        {/* this option */}
+      
+        {/* desktop option */}
         <div className='hidden lg:flex'>
 
           <ul className='flex flex-row gap-2'>
@@ -41,6 +62,24 @@ const Header = () => {
 
 
       </div>
+
+
+      {menuOpen && <div className='fixed top-0 left-0 w-full h-full bg-black ' onClick={toggleMenu}>
+
+
+        <div className=' absolute bg-white right-0 left- w-[80%] h-full  '>
+
+        <button onClick={toggleMenu} className='text-2xl focus:outline-none top-4 right-4 absolute'> <IoClose /> </button>
+
+        <ul className='flex flex-col  gap-2 my-20 mx-5 font-bold '>
+            <Link className='border p-2' to={'/'}> Home</Link>
+            <Link className='border p-2' to={'/about'}> About</Link>
+            <Link className='border p-2' to={'/media'}> Media</Link>
+            <Link className='border p-2' to={'/contactUs'}> Contact Us</Link>
+          </ul>
+
+        </div>
+      </div>}
 
 
     </header >
