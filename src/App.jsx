baf63c0fcadd14ca98ef42cd3c802adaf001dbaa
Sonu7042 <sonusinghmourya100@gmail.com>
2024-07-06 10:40:48 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './component/Header'
 import Footer from './component/Footer'
 // import { Outlet } from 'react-router-dom'
-// import About from './pages/About'
+import About from './pages/About'
 import Home from './pages/Home'
 
 
@@ -15,8 +15,8 @@ function App() {
       <Header />
       <main className='min-h-[calc(100vh-160px)] '>
         {/* <Outlet /> */}
-        {/* <About/> */}
-        <Home />
+        <About/>
+        {/* <Home /> */}
       </main>
       <Footer />
 
